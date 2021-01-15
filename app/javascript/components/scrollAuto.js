286ef0ffed60469deab5a7962fcd6866e1c59d21
Lior Levy <levy.lior.pro@gmail.com>
2021-01-15 01:51:10 +0100

    Remove unecessary utilities

--- app/javascript/components/scrollAuto.js
+++ app/javascript/components/scrollAuto.js
@@ -1,9 +1,9 @@
 const scrollAuto = () => {
-  window.setInterval(function() {
-    var elem = document.getElementById('contain');
+  const elem = document.getElementById('contain');
+
+  window.setInterval(() => {
     elem.scrollTop = elem.scrollHeight;
   }, 100);
-
 };
 
 export default scrollAuto;
